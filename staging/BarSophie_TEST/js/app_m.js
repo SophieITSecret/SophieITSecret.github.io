f115b675689075d5be360c7f355081a8397c7411
SophieITSecret <makimura@ms-forum.com>
2026-05-01 16:18:05 +0900

    Update app_m.js

--- staging/BarSophie_TEST/js/app_m.js
+++ staging/BarSophie_TEST/js/app_m.js
@@ -271,11 +271,10 @@ function showSophieMenu() {
 }
 
 function showNewsMarket() {
-    function showNewsMarket() {
     const lv = document.getElementById('list-view');
     const nm = document.getElementById('nav-main');
     if (nm) nm.style.display = 'none';
-    nav.updateNav("shop"); // ★stateをnone以外にしてdisclaimer-barを隠す
+    nav.updateNav("lq_root");
 
     const showYoutube = (videoId, label) => {
         const yt = document.getElementById('yt-wrapper');
