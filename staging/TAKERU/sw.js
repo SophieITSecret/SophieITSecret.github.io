d23763e66d6d5ecdd9f88529d358416ee823c261
SophieITSecret <makimura@ms-forum.com>
2026-09-01 06:56:52 +0900

    ps

--- staging/TAKERU/sw.js
+++ staging/TAKERU/sw.js
@@ -1,6 +1,6 @@
 // ★デプロイ(push)のたびに SW_VERSION と CACHE_NAME の番号を一緒に上げる
-const SW_VERSION = 'v119';
-const CACHE_NAME = 'takeru-v119';
+const SW_VERSION = 'v120';
+const CACHE_NAME = 'takeru-v120';
 
 const PRE_CACHE = [
     './',
